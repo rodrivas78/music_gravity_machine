fe42d7843c409aab0da299302061ba8c2dc31e43
Rodrigo Gonzales <rrivas2009@gmail.comm>
2024-12-03 20:10:41 -0300

    adiciona novos obstaculos e skybox

--- Area3d.gd
+++ Area3d.gd
@@ -5,6 +5,8 @@ extends Area3D
 @onready var som_mi_l : AudioStreamPlayer = $Mi_longo
 @onready var som_re : AudioStreamPlayer = $Re
 @onready var som_do : AudioStreamPlayer = $Do
+@onready var som_fa : AudioStreamPlayer = $Fa
+@onready var som_si : AudioStreamPlayer = $Si
 
 func _on_body_entered(body):
 	if body.is_in_group("mi_curto"):
@@ -15,3 +17,7 @@ func _on_body_entered(body):
 		som_re.play()
 	elif body.is_in_group("do"):
 		som_do.play()
+	elif body.is_in_group("fa"):
+		som_fa.play()
+	elif body.is_in_group("si"):
+		som_si.play()
